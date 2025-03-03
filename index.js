fcd043971ce0dc000db7984a0e7c54e97546ddce
jhonkill007 <jhond2069@gmail.com>
2025-03-02 21:35:12 -0400

    fix: conection string

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ import typeDefs from "./src/typeDefs.js";
 import resolvers from "./src/resolvers.js";
 import sequelize from "./src/sequelize/sequelize.js";
 
-await sequelize.sync();
+// Método para probar la conexión
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión establecida correctamente con la base de datos.');
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
+  }
+}
+
+// Llama a la función para probar la conexión
+testConnection();
 
 const server = new ApolloServer({
   typeDefs,
--- src/sequelize/sequelize.js
+++ src/sequelize/sequelize.js
@@ -1,16 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("JDDataBase01", "sa", "", {
+const sequelize = new Sequelize("graphql", "sa", "R00TP@22w0r231920", {
+  host: "94.72.114.203",
+  port: 1433,
   dialect: "mssql",
   dialectOptions: {
     options: {
-      trustedConnection: true,
-      trustServerCertificate: true,
+      trustServerCertificate: true, // Permite conexiones sin certificado SSL
     },
   },
-  host: "DESKTOP-62RJQ8L\\SQLEXPRESS",
-  logging: console.log,
-  port: 1443,
+  logging: false, // Opcional: deshabilita logs de SQL en consola
 });
 
 export default sequelize;
